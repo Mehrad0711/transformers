324a0840f53c27ecdc74628253cbe48e72d9f892
mehrad <mehrad@stanford.edu>
2020-09-01 14:10:48 -0700

    update train_mbart script

--- examples/seq2seq/train_mbart_cc25_enro.sh
+++ examples/seq2seq/train_mbart_cc25_enro.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 export PYTHONPATH="../":"${PYTHONPATH}"
 
-python finetune.py \
+# optionally
+export ENRO_DIR=/Users/Mehrad/Documents/GitHub/genienlp/tests/en-fa-test/ # Download instructions above
+# export WANDB_PROJECT="MT" # optional
+export MAX_LEN=200
+export BS=4
+export GAS=2 # gradient accumulation steps
+
+set -e
+set -x
+
+python3 finetune.py \
     --learning_rate=3e-5 \
-    --fp16 \
     --do_train \
     --val_check_interval=0.25 \
     --adam_eps 1e-06 \
-    --num_train_epochs 6 --src_lang en_XX --tgt_lang ro_RO \
+    --num_train_epochs 20 --src_lang en_XX --tgt_lang ar_AR \
     --data_dir $ENRO_DIR \
     --max_source_length $MAX_LEN --max_target_length $MAX_LEN --val_max_target_length $MAX_LEN --test_max_target_length $MAX_LEN \
     --train_batch_size=$BS --eval_batch_size=$BS \
@@ -15,4 +24,5 @@ python finetune.py \
     --warmup_steps 500 \
     --freeze_embeds \
     --model_name_or_path=facebook/mbart-large-cc25 \
+    --cache_dir /Users/Mehrad/Documents/GitHub/genienlp/.embeddings \
     "$@"
